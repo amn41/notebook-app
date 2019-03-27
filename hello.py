e6d897f017092aca7620fd8ed4eafc9709c7902c
Alan Nichol <alan@rasa.com>
2019-03-27 13:26:30 +0100

    serve static file

--- hello.py
+++ hello.py
@@ -3,17 +3,7 @@ from sanic.response import json
 
 app = Sanic()
 
-template = """
-<!DOCTYPE html>
-<html>
-<body>
-<p>This is a great webpage.</p>
-</body>
-</html>"""
-
-@app.route('/')
-async def test(request):
-    return template
+app.static('/', './static')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
